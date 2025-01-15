6ee90c027ae0689b8d7888a31a23a7c52f3396de
GiaBaorikkei <thainguyengiabao082105@gmail.com>
2025-01-16 01:14:12 +0700

    update

--- session02/bai8.sql
+++ session02/bai8.sql
@@ -5,11 +5,26 @@ use session02_database;
     - Thiếu khóa chính Primary key 
     - Thiếu ràng buộc Not null 
     - Không kiểm tra giá trị Diem
+    - Điểm là 1 thực thể yếu nên cần 2 khóa kết hợp tạo ra 1 khóa chính
 */
 
+create table sinhVien (
+    maSV int primary key,
+    ten varchar(100) not null
+);
+
+create table mon (
+    maMon int primary key,
+    tenMon varchar(200) not null
+);
+
 create table Diem (
-	maSV varchar(20) primary key not null,
+    maSV int not null,
+    maMon int not null,
     diem int not null,
-    primary key (maSV),
+    primary key (maSV, maMon),
+    foreign key (maSV) references sinhVien(maSV) on delete cascade,
+    foreign key (maMon) references mon(maMon) on delete cascade,
     check (diem >= 0 and diem <= 10)
 );
+
